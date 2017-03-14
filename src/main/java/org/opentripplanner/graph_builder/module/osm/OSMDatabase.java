7faf93f3e15bc715fb58497af8d280d723c43fe4
Hannes Junnila <hannes.junnila@gmail.com>
2017-03-14 15:34:13 +0200

    Move ring construction exception handling
    Now able to build the whole Finland graph

--- src/main/java/org/opentripplanner/graph_builder/module/osm/OSMDatabase.java
+++ src/main/java/org/opentripplanner/graph_builder/module/osm/OSMDatabase.java
@@ -659,10 +659,33 @@ public class OSMDatabase implements OpenStreetMapContentHandler {
                 }
             }
             processedAreas.add(relation);
-            try {
-                newArea(new Area(relation, outerWays, innerWays, nodesById));
-            } catch (Area.AreaConstructionException|Ring.RingConstructionException e) {
-                continue;
+
+            Map<Ring, OSMWay> wayForRing = new HashMap<>();
+            List<List<Ring>> groups = new ArrayList<>();;
+            for (OSMWay way : outerWays) {
+                Ring ring = new Ring(way.getNodeRefs(), nodesById);
+                wayForRing.put(ring, way);
+                for (List<Ring> group : groups) {
+                    try {
+                        if (ring.toJtsPolygon().intersects(group.get(0).toJtsPolygon())) {
+                            group.add(ring);
+                            break;
+                        }
+                    } catch (Ring.RingConstructionException e) {
+                        continue;
+                    }
+                }
+                groups.add(new ArrayList<>(Collections.singletonList(ring)));
+            }
+
+            for (List<Ring> group : groups) {
+                try {
+                    List<OSMWay> outerRingWays = group.stream().map(wayForRing::get)
+                        .collect(Collectors.toList());
+                    newArea(new Area(relation, outerRingWays, innerWays, nodesById));
+                } catch (Area.AreaConstructionException e) {
+                    continue;
+                }
             }
 
             for (OSMRelationMember member : relation.getMembers()) {
