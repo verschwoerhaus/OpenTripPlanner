8f10b2aa0a5c8b03679654cc514ccb43c01c26d9
Maximilian Richt <maxi@richt.name>
2019-03-06 12:33:04 +0100

    Revert "added azure dns workaround for search path"
    This reverts commit 87069171aeca2cb0f6012a308172d0782aef38c5.

--- run.sh
+++ run.sh
@@ -81,12 +81,6 @@ function process {
   build_graph $NAME $FILE
 }
 
-#workaround for azure DNS issue
-
-if [ "$EUID" -eq 0 ]
-  then echo "search marathon.l4lb.thisdcos.directory" >> /etc/resolv.conf
-fi
-
 VERSION=$(version)
 
 echo VERSION $VERSION
